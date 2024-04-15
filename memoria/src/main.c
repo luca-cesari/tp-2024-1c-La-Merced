31615046680ebbfcaf10234fa6b5a32010cd00fd
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-15 20:46:42 -0300

    memoria, implementacion parcial

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,8 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
 
-int main(int argc, char* argv[]) {
-    decir_hola("Memoria");
+#include <commons/config.h>
+
+#include <sockets/servidor.h>
+
+void atender_cliente(int32_t fd_conexion)
+{
+    // handsake (para saber quienes el cliente)
+    // ver como hacer para registrar los clientes
+    // conectados, (algun diccionario global?)
+
+    while (1)
+    {
+        // listen
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    t_config *config = config_create("memoria.config");
+    char *puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
+
+    int32_t fd_escucha = iniciar_servidor(puerto_escucha);
+
+    esperar_cliente(fd_escucha, atender_cliente);
+
     return 0;
 }
