451a9d9fb3dcc164d16fd4d9650e8c6be28ac2b2
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-16 02:06:15 -0300

    compila, pero falta test

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -5,18 +5,41 @@
 
 #include <sockets/servidor.h>
 
+void escuchar_kernel(int32_t fd_kernel)
+{
+    printf("Kernel conectado");
+}
+
+void escuchar_cpu(int32_t fd_cpu)
+{
+    printf("CPU conectado");
+}
+
+void escuchar_interfaz_es(int32_t fd_e_s)
+{
+    printf("Interfaz E/S conectada");
+}
+
 void atender_cliente(int32_t fd_conexion)
 {
     // handsake (para saber quienes el cliente)
     uint32_t modulo_cliente = recibir_cliente(fd_conexion);
-    printf("Modulo conectado: %d", modulo_cliente);
-    // ver como hacer para registrar los clientes
-    // conectados, (algun diccionario global? para asociar, modulo con fd)
 
-    // listen
-    // while (1)
-    // {
-    // }
+    switch (modulo_cliente)
+    {
+    case KERNEL:
+        escuchar_kernel(fd_conexion);
+        break;
+    case CPU:
+        escuchar_cpu(fd_conexion);
+        break;
+    case E_S:
+        escuchar_interfaz_es(fd_conexion);
+        break;
+    default:
+        printf("Error de Cliente \n");
+        return;
+    }
 }
 
 int main(void)
@@ -26,7 +49,7 @@ int main(void)
 
     int32_t fd_escucha = iniciar_servidor(puerto_escucha);
 
-    esperar_cliente(fd_escucha, atender_cliente);
+    esperar_cliente(fd_escucha, &atender_cliente);
 
     return 0;
 }
