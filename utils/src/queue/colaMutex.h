7a7b2b2519677ed711f3002fa3397da89e406db8
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-28 20:36:02 -0300

    procesos y planificador, creo

--- utils/src/queue/colaMutex.h
+++ utils/src/queue/colaMutex.h
@@ -2,14 +2,14 @@
 #include <commons/collections/queue.h>
 #include <pthread.h>
 
-typedef struct {
-    t_queue* cola;
-    pthread_mutex_t* mutex;
+typedef struct
+{
+    t_queue *cola;
+    pthread_mutex_t *mutex;
+} t_queue_mutex;
 
-}t_queue_mutex;
-
-t_queue_mutex* crear_cola_mutex(void);
-void destruir_queue_mutex(t_queue_mutex*);
-void pop_queue_mutex(t_queue_mutex* );
-void push_queue_mutex(t_queue_mutex* );
-void peek_queue_mutex(t_queue_mutex* );
+t_queue_mutex *crear_cola_mutex(void);
+void destruir_queue_mutex(t_queue_mutex *);
+void pop_queue_mutex(t_queue_mutex *);
+void push_queue_mutex(t_queue_mutex *);
+void peek_queue_mutex(t_queue_mutex *);
