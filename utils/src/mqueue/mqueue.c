373d37fb6c8da38a2805a5f33eaa8f410f378bca
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-29 13:19:49 -0300

    cambio de nombre colaMutex -> mqueue, fix definicion pop, peek y push

--- utils/src/mqueue/mqueue.c
+++ utils/src/mqueue/mqueue.c
@@ -1,14 +1,14 @@
-#include "colaMutex.h"
+#include "mqueue.h"
 
-t_queue_mutex *crear_cola_mutex()
+t_mutex_queue *create_mutex_queue()
 {
-  t_queue_mutex *cola_mutex = malloc(sizeof(t_queue_mutex));
+  t_mutex_queue *cola_mutex = malloc(sizeof(t_mutex_queue));
   cola_mutex->cola = queue_create();
   pthread_mutex_init(&(cola_mutex->mutex), NULL);
   return cola_mutex;
 }
 
-void pop_cola_mutex(t_queue_mutex *cola_mutex)
+void *pop_mutex_queue(t_mutex_queue *cola_mutex)
 {
   pthread_mutex_lock(&(cola_mutex->mutex));
   void *contenido = queue_pop(cola_mutex->cola);
@@ -16,24 +16,24 @@ void pop_cola_mutex(t_queue_mutex *cola_mutex)
   return contenido;
 }
 
-void push_cola_mutex(t_queue_mutex *cola_mutex, void *contenido)
+void *push_mutex_queue(t_mutex_queue *cola_mutex, void *contenido)
 {
   pthread_mutex_lock(&(cola_mutex->mutex));
   queue_push(cola_mutex->cola, contenido);
   pthread_mutex_unlock(&(cola_mutex->mutex));
 }
 
-void destruir_cola_mutex(t_queue_mutex *cola_mutex)
+void destruir_mutex_queue(t_mutex_queue *cola_mutex)
 {
   pthread_mutex_lock(&(cola_mutex->mutex));
   queue_destroy(cola_mutex->cola);
-  pthread_mutex_unlock(&(cola_mutex->mutex));
+  pthread_mutex_unlock(&(cola_mutex->mutex)); // ver si hace falte el unlock
   pthread_mutex_destroy(&(cola_mutex->mutex));
   free(cola_mutex);
 }
 
 // Ver si se usa
-void peek_cola_mutex(t_queue_mutex *cola_mutex)
+void *peek_mutex_queue(t_mutex_queue *cola_mutex)
 {
   pthread_mutex_lock(&(cola_mutex->mutex));
   void *contenido = queue_peek(cola_mutex);
---
+++ utils/src/mqueue/mqueue.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <commons/collections/queue.h>
+#include <pthread.h>
+
+typedef struct
+{
+    t_queue *cola;
+    pthread_mutex_t *mutex;
+} t_mutex_queue;
+
+t_mutex_queue *create_mutex_queue(void);
+void *pop_mutex_queue(t_mutex_queue *);
+void *peek_mutex_queue(t_mutex_queue *);
+void push_mutex_queue(t_mutex_queue *, void *);
+void destruir_mutex_queue(t_mutex_queue *);
