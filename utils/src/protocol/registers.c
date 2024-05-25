10e4aad8661150396b53c42b9a416a9f75af5e1e
Melisa Selene Perez <melperez@frba.utn.edu.ar>
2024-05-25 15:11:40 +0000

    serializacion

--- utils/src/protocol/registers.c
+++ utils/src/protocol/registers.c
@@ -1,7 +1,23 @@
 #include "registers.h"
 
-t_registers_generales crear_registros()
+t_registers_generales *crear_registros()
 {
-   t_registers_generales registros;
+   t_registers_generales *registros = malloc(sizeof(t_registers_generales);
+
+
+   registros->AX = 0;
+   registros->BX = 0;
+   registros->CX = 0;
+   registros->DX = 0;
+   registros->EAX = 0;
+   registros->EBX = 0;
+   registros->ECX = 0;
+   registros->EBX = 0;
+
    return registros;
+}
+
+void destruir_registers_generales(t_registers_generales *registros)
+{
+   free(registros);
 }
