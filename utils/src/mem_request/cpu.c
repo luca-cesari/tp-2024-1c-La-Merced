181ac3367421722f93e4d3164ea88aa45decfd2b
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-06-08 22:47:32 +0000

    cambio pcb por pc y pid

--- utils/src/mem_request/cpu.c
+++ utils/src/mem_request/cpu.c
@@ -1,17 +1,17 @@
 #include "cpu.h"
 
-t_cpu_mem_req *crear_cpu_mem_request(cpu_req_operation operacion, t_pcb pcb, u_int32_t pid, u_int32_t nro_pag)
+t_cpu_mem_req *crear_cpu_mem_request(cpu_req_operation operacion, int32_t program_counter, u_int32_t pid, u_int32_t nro_pag)
 {
    t_cpu_mem_req *mem_request = malloc(sizeof(t_cpu_mem_req));
    mem_request->operacion = operacion;
+   mem_request->pid = pid;
    switch (operacion)
    {
    case FETCH_INSTRUCCION:
-      mem_request->parametros.pcb = pcb;
+      mem_request->parametros.program_counter = program_counter;
       break;
    case OBTENER_MARCO:
-      mem_request->parametros.obt_marco_parametros.pid = pid;
-      mem_request->parametros.obt_marco_parametros.nro_pag = nro_pag;
+      mem_request->parametros.nro_pag = nro_pag;
       break;
    default:
       break;
@@ -25,10 +25,18 @@ void enviar_cpu_mem_request(int32_t fd_memoria, t_cpu_mem_req *mem_request)
    t_packet *paquete = crear_paquete();
    crear_buffer(paquete);
    agregar_a_paquete(paquete, &(mem_request->operacion), sizeof(t_cpu_mem_req));
-   agregar_a_paquete(paquete, &(mem_request->parametros.pcb), sizeof(t_pcb));
-   agregar_a_paquete(paquete, &(mem_request->parametros.obt_marco_parametros.pid), sizeof(u_int32_t));
-   agregar_a_paquete(paquete, &(mem_request->parametros.obt_marco_parametros.nro_pag), sizeof(u_int32_t));
-
+   agregar_a_paquete(paquete, &(mem_request->pid), sizeof(u_int32_t));
+   switch (mem_request->operacion)
+   {
+   case FETCH_INSTRUCCION:
+      agregar_a_paquete(paquete, &(mem_request->parametros.program_counter), sizeof(u_int32_t));
+      break;
+   case OBTENER_MARCO:
+      agregar_a_paquete(paquete, &(mem_request->parametros.nro_pag), sizeof(u_int32_t));
+      break;
+   default:
+      break;
+   }
    enviar_paquete(paquete, fd_memoria);
    eliminar_paquete(paquete);
 }
@@ -39,15 +47,15 @@ t_cpu_mem_req *recibir_cpu_mem_request(int32_t fd_cpu)
    t_cpu_mem_req *mem_request = malloc(sizeof(t_cpu_mem_req));
 
    mem_request->operacion = *(cpu_req_operation *)list_get(paquete, 0);
+   mem_request->pid = *(u_int32_t *)list_get(paquete, 1);
 
    switch (mem_request->operacion)
    {
    case FETCH_INSTRUCCION:
-      mem_request->parametros.pcb = *(t_pcb *)list_get(paquete, 1);
+      mem_request->parametros.program_counter = *(int32_t *)list_get(paquete, 2);
       break;
    case OBTENER_MARCO:
-      mem_request->parametros.obt_marco_parametros.pid = *(u_int32_t *)list_get(paquete, 2);
-      mem_request->parametros.obt_marco_parametros.nro_pag = *(u_int32_t *)list_get(paquete, 3);
+      mem_request->parametros.nro_pag = *(u_int32_t *)list_get(paquete, 3);
       break;
    default:
       break;
@@ -59,7 +67,7 @@ t_cpu_mem_req *recibir_cpu_mem_request(int32_t fd_cpu)
 
 void destruir_cpu_mem_request(t_cpu_mem_req *mem_request)
 {
-   // EN PRINCIPIO NO SE USA
+   // VER SI SE USA
    // switch (mem_request->operacion)
    // {
    // case FETCH_INSTRUCCION:
--- utils/src/mem_request/cpu.h
+++ utils/src/mem_request/cpu.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_MEM_REQUEST_CPU_H
 #define UTILS_MEM_REQUEST_CPU_H
 
-#include <pcb/pcb.h>
-
+#include <stdlib.h>
+#include <protocol/protocol.h>
 typedef enum
 {
    FETCH_INSTRUCCION,
@@ -11,21 +11,16 @@ typedef enum
 
 typedef struct
 {
-   u_int32_t pid;
-   u_int32_t nro_pag;
-} obt_marco_parametros;
-
-typedef struct
-{
    cpu_req_operation operacion;
+   u_int32_t pid;
    union
    {
-      t_pcb pcb;                                 // Presente solo para FETCH_INSTRUCCION
-      obt_marco_parametros obt_marco_parametros; // Presente solo para OBTENER_MARCO
+      int32_t program_counter; // Presente solo para FETCH_INSTRUCCION
+      u_int32_t nro_pag;       // Presente solo para OBTENER_MARCO
    } parametros;
 } t_cpu_mem_req;
 
-t_cpu_mem_req *crear_cpu_mem_request(cpu_req_operation operacion, t_pcb pcb, u_int32_t pid, u_int32_t nro_pag);
+t_cpu_mem_req *crear_cpu_mem_request(cpu_req_operation operacion, int32_t program_counter, u_int32_t pid, u_int32_t nro_pag);
 void enviar_cpu_mem_request(int32_t fd_memoria, t_cpu_mem_req *mem_request);
 t_cpu_mem_req *recibir_cpu_mem_request(int32_t fd_cpu);
 void destruir_cpu_mem_request(t_cpu_mem_req *mem_request);
