86e253f21714b4cdadd335ebad6cd788255b5edb
luca-cesari <lucacesari2002@gmail.com>
2024-06-08 23:14:03 +0000

    Agrego lógica para escribir en memoria a pesar de que se supere el limite del frame

--- memoria/src/mem_usuario/mem_usuario.c
+++ memoria/src/mem_usuario/mem_usuario.c
@@ -124,16 +124,64 @@ u_int32_t get_cantidad_frames_disponibles()
     }
     return cantidad_frames_disponibles;
 }
+/*
+Acceso a espacio de usuario
+Esta petición puede venir tanto de la CPU como de un Módulo de Interfaz de I/O, es importante tener en cuenta que las peticiones pueden ocupar más de una página.
+El módulo Memoria deberá realizar lo siguiente:
+Ante un pedido de lectura, devolver el valor que se encuentra a partir de la dirección física pedida.
+Ante un pedido de escritura, escribir lo indicado a partir de la dirección física pedida. En caso satisfactorio se responderá un mensaje de ‘OK’.
+*/
 
-void escribir_memoria_usuario(u_int32_t direccion_fisica, void *buffer, u_int32_t tamanio)
+void escribir_memoria_usuario(u_int32_t pid, u_int32_t direccion_fisica, void *buffer, u_int32_t tamanio_buffer)
 {
-    memcpy(memoria_usuario + direccion_fisica, buffer, tamanio);
+    u_int32_t frame = get_numero_de_frame(direccion_fisica);
+    u_int32_t limite_de_frame = frame * get_tamanio_pagina() + get_tamanio_pagina();
+    u_int32_t tamanio_guardado = 0;
+
+    while ((direccion_fisica < limite_de_frame) && (tamanio_guardado < tamanio_buffer))
+    {
+        memcpy(memoria_usuario + direccion_fisica, buffer, 1); // Va copiando byte por byte del buffer a la memoria
+        direccion_fisica++;
+        buffer++;
+    }
+    if (tamanio_guardado == tamanio_buffer)
+    {
+        // OK
+    }
+    else
+    {
+        // LOGICA PARA SEGUIR ESCRIBIENDO
+        u_int32_t nro_pag_sig = obtener_numero_pagina_siguiente(pid, frame);
+        // LOGICA PARA DEVOLVER A CPU EL NUMERO DE PAGINA SIGUIENTE
+    }
 }
 
-void leer_memoria_usuario(u_int32_t direccion_fisica, void *buffer, u_int32_t tamanio)
+u_int32_t obtener_numero_pagina_siguiente(u_int32_t pid, u_int32_t frame)
+{
+    t_proceso_y_tabla *proceso_y_tabla = list_get(lista_tablas, pid);
+    u_int32_t nro_pagina_actual;
+
+    t_list_iterator *iterador = list_iterator_create(proceso_y_tabla->tabla_paginas);
+
+    while (list_iterator_has_next(iterador))
+    {
+        u_int32_t *nro_frame = (u_int32_t *)list_iterator_next(iterador);
+        if (*nro_frame == frame)
+        {
+            nro_pagina_actual = list_iterator_index(iterador);
+            break;
+        }
+    }
+
+    list_iterator_destroy(iterador);
+
+    return nro_pagina_actual + 1;
+}
+
+/*void leer_memoria_usuario(u_int32_t pid, u_int32_t direccion_fisica, void *buffer, u_int32_t tamanio)
 {
     memcpy(buffer, memoria_usuario + direccion_fisica, tamanio);
-}
+}*/
 
 void destruir_memoria_usuario()
 {
