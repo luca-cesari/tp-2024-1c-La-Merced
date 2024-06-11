a483c1f612ae19fc34800b09c51e3fa5efee964d
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-06-11 05:18:33 +0000

    algunas cosas de la instruccion stdin read

--- entradasalida/src/interfaz/stdin.c
+++ entradasalida/src/interfaz/stdin.c
@@ -7,7 +7,7 @@ void iniciar_rutina_interfaz_stdin()
     {
         t_io_request *peticion_io = esperar_instruccion();
 
-        if (strcmp(peticion_io->instruction, "IO_STDIN_READ")!=0)
+        if (strcmp(peticion_io->instruction, "IO_STDIN_READ") != 0)
             enviar_respuesta(INVALID_INSTRUCTION);
 
         io_stdin_read(peticion_io->arguments);
@@ -16,7 +16,11 @@ void iniciar_rutina_interfaz_stdin()
     }
 }
 
-void io_stdin_read(char *) //RECIBE COMO PARÁMETRO UNA O VARIAS DIRECCIONES FISICAS
+void io_stdin_read(char *argumentos)
 {
-    //ACA VA LA LOGICA DE STDIN
+    char **parametros = string_split(argumentos, " ");
+    int tamanio_valor = atoi(parametros[0]);
+    char *valor_consola = iniciar_consola(tamanio_valor);
+
+    //mem request
 }
