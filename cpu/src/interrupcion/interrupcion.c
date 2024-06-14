a695708b44d5c7ec211486d59f9f0f2271c06a82
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-06-14 14:43:06 +0000

    mutex

--- cpu/src/interrupcion/interrupcion.c
+++ cpu/src/interrupcion/interrupcion.c
@@ -3,19 +3,24 @@
 int8_t _hay_interrupcion;
 pthread_mutex_t hay_interrupcion_mutex;
 int32_t interrupcion;
+pthread_mutex_t interrupcion_mutex;
 
 void inicializar_interrupcion()
 {
    _hay_interrupcion = 0;
+   interrupcion = -1;
    pthread_mutex_init(&hay_interrupcion_mutex, NULL);
+   pthread_mutex_init(&interrupcion_mutex, NULL);
 }
 
 void set_interrupcion(int32_t tipo)
 {
    pthread_mutex_lock(&hay_interrupcion_mutex);
    _hay_interrupcion = 1;
-   interrupcion = tipo;
    pthread_mutex_unlock(&hay_interrupcion_mutex);
+   pthread_mutex_lock(&interrupcion_mutex);
+   interrupcion = tipo;
+   pthread_mutex_unlock(&interrupcion_mutex);
 }
 
 void reset_interrupcion()
@@ -27,14 +32,17 @@ void reset_interrupcion()
 
 int8_t hay_interrupcion()
 {
-   int8_t interrupcion;
+   int8_t hay_interrupt;
    pthread_mutex_lock(&hay_interrupcion_mutex);
-   interrupcion = _hay_interrupcion;
+   hay_interrupt = _hay_interrupcion;
    pthread_mutex_unlock(&hay_interrupcion_mutex);
-   return interrupcion;
+   return hay_interrupt;
 }
 
 int32_t tipo_interrupcion()
 {
-   return interrupcion;
+   pthread_mutex_lock(&interrupcion_mutex);
+   int32_t interruption = interrupcion;
+   pthread_mutex_unlock(&interrupcion_mutex);
+   return interruption;
 }
