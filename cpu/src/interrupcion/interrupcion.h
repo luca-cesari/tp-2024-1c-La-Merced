6d8d366a27ec43937b4f2c1e1f2f0aaa2feef5f1
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-06-14 13:21:11 +0000

    tipo de interrupcion

--- cpu/src/interrupcion/interrupcion.h
+++ cpu/src/interrupcion/interrupcion.h
@@ -5,8 +5,9 @@
 #include <pthread.h>
 
 void inicializar_interrupcion(void);
-void set_interrupcion(void);
+void set_interrupcion(int32_t);
 void reset_interrupcion(void);
 int8_t hay_interrupcion(void);
+int32_t tipo_interrupcion();
 
 #endif // INTERRUPCION_H
