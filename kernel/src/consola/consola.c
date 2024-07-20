5452955822534b184ae35f88c168eec2f1811e2f
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-07-20 22:49:19 +0000

    historial y autocompletado para comandos en kernel

--- kernel/src/consola/consola.c
+++ kernel/src/consola/consola.c
@@ -5,16 +5,40 @@ static void ejecutar_comando(char *, char *);
 static void imprimir_procesos();
 static void destruir_proceso(void *);
 
+static const char *comandos[] = {
+    INICIAR_PLANIFICACION,
+    DETENER_PLANIFICACION,
+    PROCESO_ESTADO,
+    EJECUTAR_SCRIPT,
+    INICIAR_PROCESO,
+    FINALIZAR_PROCESO,
+    MULTIPROGRAMACION,
+    NULL};
+
+
+void inicializar_readline()
+{
+   rl_readline_name = "Consola";
+   rl_attempted_completion_function = completar_comando;
+}
+
 void iniciar_consola()
 {
    char *comando = NULL;
    char *operacion = NULL;
    char *argumento = NULL;
 
+   inicializar_readline();
+
    while (1)
    {
       comando = readline("> ");
       char **vec_comando = string_split(comando, " ");
+
+      if (comando)
+      {
+         add_history(comando);
+      }
 
       if (vec_comando[0] == NULL)
       {
@@ -148,4 +172,42 @@ static void destruir_proceso(void *proceso)
 {
    t_pcb *pcb = (t_pcb *)proceso;
    destruir_pcb(pcb);
+}
+
+// Función para autocompletar comandos
+char **completar_comando(const char *texto, int inicio, int fin)
+{
+   char **coincidencias;
+   if (inicio == 0)
+   {
+      coincidencias = rl_completion_matches(texto, generador_comando);
+   }
+   else
+   {
+      coincidencias = NULL;
+   }
+   return coincidencias;
+}
+
+// Generador de comandos para autocompletado
+char *generador_comando(const char *texto, int estado)
+{
+   static int indice;
+   static int longitud;
+   const char *comando;
+
+   if (!estado)
+   {
+      indice = 0;
+      longitud = strlen(texto);
+   }
+
+   while ((comando = comandos[indice++]))
+   {
+      if (strncmp(comando, texto, longitud) == 0)
+      {
+         return strdup(comando);
+      }
+   }
+   return NULL;
 }
