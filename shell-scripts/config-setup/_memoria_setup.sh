221a3095ef59421c1f23e2750aca2bc6b4d44e1a
Jiun Ming Hsu <hsujm219@gmail.com>
2024-07-27 10:12:06 -0300

    script memoria

--- shell-scripts/config-setup/_memoria_setup.sh
+++ shell-scripts/config-setup/_memoria_setup.sh
@@ -3,8 +3,12 @@
 cd ../memoria/
 rm -f *.config
 
-echo "PUERTO_ESCUCHA=8002" >> memoria.config
-echo "" >> memoria.config
-echo "PATH_INSTRUCCIONES=/home/utnso/scripts-pruebas" >> memoria.config
-echo "" >> memoria.config
+cd ../c-comenta-pruebas
+cwd=$(pwd)
 
+cd ../memoria/
+
+echo "PUERTO_ESCUCHA=8002" >>memoria.config
+echo "" >>memoria.config
+echo "PATH_INSTRUCCIONES=${cwd}" >>memoria.config
+echo "" >>memoria.config
